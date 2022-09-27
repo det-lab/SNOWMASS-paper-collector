a7e27db3644a613cd8c82480364aadd5d64530af
Amy Roberts <cantor.duster@gmail.com>
2022-09-27 12:57:58 -0600

    update scraping script

--- get-pdfs.py
+++ get-pdfs.py
@@ -1,6 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'
+}
 
 URL = "https://snowmass21.org/submissions/ef"
-page = requests.get(URL)
+page = requests.get(URL, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
 
-print(page.text)
+#print(page.text)
+for div in soup.find_all('div', {'class': 'li'}):
+    print(div.text)
+    for link in div.find_all('a'):
+        href = link.get('href')
+        if 'pdf' in href:
+            print(href)
+
